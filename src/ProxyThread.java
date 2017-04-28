c7642ee7b60103f6dd677920f524eefc9a012edd
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-04-28 03:03:48 -0400

    remove prints and added some comments

--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -16,36 +18,39 @@ public class ProxyThread extends Thread {
     @Override
     public void run() {
         try {
+            // Read request for browser
             InputStream inputStream = socket.getInputStream();
             StringBuilder requestBuilder = new StringBuilder();
             byte[] bytes = new byte[1024];
             int read = inputStream.read(bytes);
-            while(read != -1) {
+            while (read != -1) {
                 requestBuilder.append(new String(bytes));
                 if (requestBuilder.toString().contains("\r\n\r\n")) break;
                 read = inputStream.read(bytes);
 
             }
-
             String request = requestBuilder.toString();
-            if (request.equals("")) return;
-            System.out.println("~~~~~~~~~~~~ Received HTTP Request ~~~~~~~~~~~~");
-            System.out.println(request);
+            if (request.equals("")) return; //Dunno why that happens. Burn it.
+//            System.out.println("~~~~~~~~~~~~ Received HTTP Request ~~~~~~~~~~~~");
+//            System.out.println(request);
 
 
+            //Extract host from request
             String hostname = extractHostname(request).trim();
             String requestToSend = removeHostnameFromGet(request, hostname);
             Socket proxySocket = new Socket(InetAddress.getByName(hostname), 80);
+            // forward request to website
             PrintWriter pw = new PrintWriter(proxySocket.getOutputStream());
             pw.write(requestToSend);
             pw.flush();
 
+            //Throw response back to the client
             InputStream in = proxySocket.getInputStream();
-            byte[] arr = new byte[1024*1024];
+            byte[] arr = new byte[1024 * 1024];
             int byteRead = in.read(arr);
-            while(byteRead != -1) {
+            while (byteRead != -1) {
                 socket.getOutputStream().write(arr, 0, byteRead);
-                System.out.print(new String(arr));
+//                System.out.print(new String(arr));
                 byteRead = in.read(arr);
             }
 
@@ -58,7 +63,7 @@ public class ProxyThread extends Thread {
     }
 
     private String removeHostnameFromGet(String request, String hostname) {
-        return request.replace("http://"+hostname, "");
+        return request.replace("http://" + hostname, "");
     }
 
     private String extractHostname(String request) {
